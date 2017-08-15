9e5026d4a256c0d5875c3962de57eb05b82c174e
STENUNGSUND\aron.tornqvist <arontornquist@hotmail.com>
2017-08-15 15:15:00 +0200

    Uppdateringar

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,47 +25,46 @@ namespace DivideGroups
             bool controll = true;
             List<Person> listOfPersons = new List<Person>();
             Console.Write("Vill du läsa in din namnlista från fil (filformat: Kalle m, Lisa k etc), y/n: ");
-            string respons = Console.ReadLine();
-
-            Console.Write("Önskas en könsfördelad lista (y/n): ");
-            if (Console.ReadLine().ToLower().Equals("y"))
+            string response = Console.ReadLine().ToLower();
+            do
             {
-                do
+                List<string> tempList = new List<string>();
+                controll = true;
+                if (response == "y")
                 {
-                    controll = true;
-                    Console.Write("Ange alla personer inklusive kön (ex Lisa k, Kalle m,): ");
-                    List<string> tempList = Console.ReadLine().Split(',').ToList();
-                    foreach (var person in tempList)
-                    {
-                        string[] a = person.Trim().Split(' ');
-                        if (a.Length == 2)
-                        {
-                            Gender tempGender = ParseGender(person);
-                            Person x = new Person(a[0], tempGender);
-                            listOfPersons.Add(x);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Felinmatning, försök igen!");
-                            Console.WriteLine();
-                            controll = false;
-                        }
-                    }
-                } while (!controll);
-            }
-            else
-            {
-                
-                Console.Write("Ange alla personer (ex Lisa,Kalle): ");
-                List<string> tempList = Console.ReadLine().Split(',').ToList(); //, StringSplitOptions.RemoveEmptyEntries);
-                foreach (var person in tempList)
-                {    
-                    Gender tempGender = ParseGender(person);
-                    Person x = new Person(person, tempGender);
-                    listOfPersons.Add(x);     
+                    tempList = File.ReadAllText("nameList.txt").Split(',').ToList();
                 }
-                
-            }
+                else
+                {
+                    Console.Write("Önskas en könsfördelad lista (y/n): ");
+                    if (Console.ReadLine().ToLower().Equals("y"))
+                    {
+                        Console.Write("Ange alla personer inklusive kön (ex Lisa k, Kalle m,): ");
+                        tempList = Console.ReadLine().Split(',').ToList();
+                    }
+                    else
+                    {
+                        Console.Write("Ange alla personer (ex Lisa, Kalle): ");
+                        tempList = Console.ReadLine().Split(',').ToList();
+                    }
+                }
+                foreach (var person in tempList)
+                {
+                    string[] a = person.Trim().Split(' ');
+                    if (a.Length == 2)
+                    {
+                       Gender tempGender = ParseGender(person);
+                       Person x = new Person(a[0], tempGender);
+                       listOfPersons.Add(x);
+                    }
+                    else
+                    {
+                       Console.WriteLine("Felinmatning, försök igen!");
+                       Console.WriteLine();
+                       controll = false;
+                    }
+                }
+            } while (!controll);
             return listOfPersons;
         }
     
@@ -73,12 +73,17 @@ namespace DivideGroups
         {
             string[] a = person.Trim().Split(' ');
 
-            if (a[1].ToLower().Equals("k"))
-                return Gender.Female;
-            else if (a[1].ToLower().Equals("m"))
-                return Gender.Male;
+            if (a.Length >= 2)
+            {
+                if (a[1].ToLower().Equals("k"))
+                    return Gender.Female;
+                else if (a[1].ToLower().Equals("m"))
+                    return Gender.Male;
+                else
+                    return Gender.Other;
+            }
             else
-                return Gender.Other;
+                return Gender.Unknown;
         }
 
         private static void PrintGroups(List<Group> sortedGroups)
